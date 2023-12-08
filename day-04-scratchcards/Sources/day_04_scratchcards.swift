f23539337f39180bfbbfba95631df7c94ef63e99
Josh <59149060+j-o-sh@users.noreply.github.com>
2023-12-08 17:03:36 +0100

    Seems working

--- day-04-scratchcards/Sources/day_04_scratchcards.swift
+++ day-04-scratchcards/Sources/day_04_scratchcards.swift
@@ -26,24 +26,58 @@ func asNumbers(_ str: String?) -> [Int] {
     return str.split(separator: " ").compactMap { Int($0) }
 }
 
+struct Card {
+    let label: String
+    let winning: [Int]
+    let scratched: [Int]
+    
+    var matching: Int { return scratched.filter(winning.contains).count }
+    var total: Decimal { return matching == 0 ? 0 : pow(2, matching - 1) }
+}
+
+extension Card {
+    init(label: String?, winning: String?, mine: String?) {
+        self.label = label ?? ""
+        self.winning = asNumbers(winning)
+        self.scratched = asNumbers(mine)
+    }
+}
+
 @main
 struct day_04_scratchcards: ParsableCommand {
     @Argument
     var filename: String
 
+    @Flag
+    var copyRules: Bool = false
+
     mutating func run() throws {
         let content = try String(contentsOfFile: filename)
-        let result = content.split(separator: "\n")
+        let cards = content.split(separator: "\n")
             .map(splitIntoChunks(":", "|"))
-            .map { ($0[0], $0[1], $0[2]) }
-            .map { (label, winning, mine) in (
-                label ?? "",
-                asNumbers(winning),
-                asNumbers(mine)
-            )}
-            .map { (lbl, win, mine) in mine.filter(win.contains) }
-            .map { mine in mine.isEmpty ? 0 : pow(2, mine.count - 1) }
+            .map { Card(label: $0[0] ?? "", winning: $0[1], mine: $0[2]) }
+
+
+        if (copyRules) {
+            var stack = cards.map { ( card: $0, copies: 1 ) }
+            for i in 0..<stack.count - 1 {
+                let (card, copies) = stack[i]
+                let reach = (i + 1)..<min(card.matching + i + 1, stack.count)
+                for j in reach { stack[j].copies += copies }
+            }
+            for (card, copies) in stack {
+                print("\(copies) * \(card.label) - \(card.matching) matching")
+            }
+            print("----")
+            print("Total: \(stack.reduce(0, { $0 + $1.copies }))")
+        } else {
+            print("Winning Total: \(doublingPoints(cards))")
+        }
+    }
+
+    func doublingPoints(_ cards: [Card]) -> Decimal {
+        return cards
+            .map { $0.total }
             .reduce(0, +)
-        print("Winning Total: \(result)")
     }
 }
